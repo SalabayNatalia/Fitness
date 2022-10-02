83262e1e3d644af2009653eaca8b84627ce4f595
unknown <salabay.natalia89@gmail.com>
2022-10-02 23:34:21 +0300

    Начала делать слайдеры (coaches, feedback)

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,15 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
+new Swiper('.feedback__slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+
+new Swiper('.coaches__slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
