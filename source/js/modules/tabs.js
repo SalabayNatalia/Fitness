d4dad2a3432f52ea0c9efb8e4c4027407ce3bc24
unknown <salabay.natalia89@gmail.com>
2022-10-06 23:44:40 +0300

    Правки после код-ревью

--- source/js/modules/tabs.js
+++ source/js/modules/tabs.js
@@ -4,24 +4,55 @@ const activateTab = function () {
   let tabName;
   if (tabNav) {
     tabNav.forEach((item) => {
-      item.addEventListener('click', selectTabNav);
+      item.addEventListener('click', () => selectTabNav(item));
     });
   }
-  function selectTabNav() {
+  function selectTabNav(selectedItem) {
     tabNav.forEach((item) => {
       item.classList.remove('is-active');
     });
-    this.classList.add('is-active');
-    tabName = this.getAttribute('data-tab-name');
+    selectedItem.classList.add('is-active');
+    tabName = selectedItem.getAttribute('data-tab-name');
     selectTabContent(tabName);
   }
 
-  function selectTabContent(tabName) {
+  function selectTabContent(selectedTabName) {
     tabContent.forEach((item) => {
-      item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+      if (item.classList.contains(selectedTabName)) {
+        item.classList.add('is-active');
+      } else {
+        item.classList.remove('is-active');
+      }
     });
   }
 };
 
-
 export {activateTab};
+
+// const activateTab = function () {
+//   const tabNav = document.querySelectorAll('.tabs-nav__item');
+//   const tabContent = document.querySelectorAll('.tab');
+//   let tabName;
+//   if (tabNav) {
+//     tabNav.forEach((item) => {
+//       item.addEventListener('click', selectTabNav);
+//     });
+//   }
+//   function selectTabNav() {
+//     tabNav.forEach((item) => {
+//       item.classList.remove('is-active');
+//     });
+//     this.classList.add('is-active');
+//     tabName = this.getAttribute('data-tab-name');
+//     selectTabContent(tabName);
+//   }
+
+//   function selectTabContent(tabName) {
+//     tabContent.forEach((item) => {
+//       item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+//     });
+//   }
+// };
+
+
+// export {activateTab};
